42d9de56ae820eb340f1e494e7e0e41b56270ff6
walls38 <walls38@purdue.edu>
2025-02-02 22:13:47 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function lotteryNumber(){
+    let num=(1 + Math.floor(Math.random()* 49))
+    return(`${num}`)
+};
+let getSixNumbers = function () {
+    for(let lotteryNumber= 1; lotteryNumber <=6;lotteryNumber++) {
+        console.log((1 + Math.floor(Math.random()* 49)))
+    }
+};
+  console.log(lotteryNumber());
+  getSixNumbers();
+    
